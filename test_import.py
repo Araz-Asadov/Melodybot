3897e705d995c6ff4f3af969b44efba4224cbd46
Araz <arazasadov041@gmail.com.>
2025-10-07 16:36:11 +0400

    test_import.py  LF -> CRLF warning handled

---
+++ test_import.py
@@ -0,0 +1,7 @@
+﻿import aiogram
+import sqlite3
+from fuzzywuzzy import fuzz
+from rapidfuzz.distance import Levenshtein
+
+print("Bütün kitabxanalar quraşdırılıb və işləyir!")
+print(f"Aiogram versiyası: {aiogram.__version__}")
